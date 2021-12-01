9e376871ba1033135c66c2dbaad5784208f16663
xuxunmi <xuxunmi520@163.com>
2021-12-01 15:41:40 +0800

    xxm:20211201：main.js引入mockjs

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './router';
 import store from './store';
 import './plugins/element.js';
 import '@/styles/global.css';
+import '@/mock/index.js';
 
 Vue.config.productionTip = false;
 
