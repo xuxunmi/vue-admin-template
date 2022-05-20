ed143acb82eb56c142749573c3add838addac372
xuxunmi <pt-xxm@pt-china.com.cn>
2022-05-20 20:50:05 +0800

    xxm:20220520：在生产环境中bulid去除报错

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,20 @@
-module.exports = {
-  presets: ['@vue/cli-plugin-babel/preset'],
-  plugins: [
-    [
-      'component',
-      {
-        libraryName: 'element-ui',
-        styleLibraryName: 'theme-chalk'
-      }
+let config = {
+    presets: ['@vue/cli-plugin-babel/preset'],
+    plugins: [
+        [
+            'component',
+            {
+                libraryName: 'element-ui',
+                styleLibraryName: 'theme-chalk'
+            }
+        ]
     ]
-  ]
+};
+
+// 在生产环境中 bulid  去除报错：
+// Unexpected console statement (no-console) 去除 console 中的插件
+if (process.env.NODE_ENV === 'production') {
+    config.plugins.push('transform-remove-console');
 }
+
+module.exports = config;
