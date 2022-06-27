571328b86c47e5d40787f241002bc4cbd2173ccb
xuxunmi <pt-xxm@pt-china.com.cn>
2022-06-27 09:06:57 +0800

    fix:修复tailwindcss配置文件问题

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,10 @@
 module.exports = {
-    content: ['./src/**/*.{html,vue,js,ts,jsx,tsx}'],
+    content: ['./public/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
     theme: {
         extend: {}
+    },
+    corePlugins: {
+        preflight: false
     },
     plugins: []
 };
