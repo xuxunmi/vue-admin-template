554c7d0063e9b0add323357d9a571b06c974bd65
xuxunmi <pt-xxm@pt-china.com.cn>
2022-07-05 15:29:34 +0800

    fix:20220705：优化

--- src/views/Layout/components/LayoutMain.vue
+++ src/views/Layout/components/LayoutMain.vue
@@ -1,9 +1,9 @@
 <template>
     <!-- 路由占位符 -->
-    <!-- 不能使用 keep-alive 缓存组件，会导致canvas报错 -->
-
     <!-- 组件过渡 -->
     <transition name="component-fade" mode="out-in">
+        <!-- 不能使用 keep-alive 缓存组件，会导致canvas报错 -->
+        <!-- keep-alive：include属性不能缓存嵌套的组件 -->
         <!-- <keep-alive> -->
         <router-view :key="key"></router-view>
         <!-- </keep-alive> -->
