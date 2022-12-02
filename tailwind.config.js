bc2809bb1c4af48e9a47361272d0839b9a08e87c
xuxunmi <pt-xxm@pt-china.com.cn>
2022-12-02 17:48:07 +0800

    fix:20221202:tailwind.config.js优化

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-    content: ['./public/*.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+    purge: {
+        content: ['./public/*.html', './src/**/*.{vue,js,ts,jsx,tsx}']
+    },
     theme: {
         extend: {}
     },
