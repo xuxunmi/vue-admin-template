67e17de9a724b1422522e1d0a3816c593d231e5d
xuxunmi <pt-xxm@pt-china.com.cn>
2024-03-07 17:27:38 +0800

    feat:20240307:js加减乘除精度问题工具函数

---
+++ src/utils/decimalPrecision.js
@@ -0,0 +1,78 @@
+/**
+ * js加减乘除精度问题
+ */
+// 加法
+export const accAdd = (arg1, arg2) => {
+    var r1, r2, m;
+    try {
+        r1 = arg1.toString().split('.')[1].length;
+    } catch (e) {
+        r1 = 0;
+    }
+    try {
+        r2 = arg2.toString().split('.')[1].length;
+    } catch (e) {
+        r2 = 0;
+    }
+    m = Math.pow(10, Math.max(r1, r2));
+    return (arg1 * m + arg2 * m) / m;
+};
+
+// 减法
+export const subtr = (arg1, arg2) => {
+    var r1, r2, m, n;
+    try {
+        r1 = arg1.toString().split('.')[1].length;
+    } catch (e) {
+        r1 = 0;
+    }
+    try {
+        r2 = arg2.toString().split('.')[1].length;
+    } catch (e) {
+        r2 = 0;
+    }
+    m = Math.pow(10, Math.max(r1, r2));
+    //last modify by deeka
+    //动态控制精度长度
+    n = r1 >= r2 ? r1 : r2;
+    return ((arg1 * m - arg2 * m) / m).toFixed(n);
+};
+
+// 乘法获取准确值
+export const accMul = (arg1, arg2) => {
+    var m = 0,
+        s1 = arg1.toString(),
+        s2 = arg2.toString();
+    try {
+        m += s1.split('.')[1].length;
+    } catch (e) {
+        console.log(e);
+    }
+    try {
+        m += s2.split('.')[1].length;
+    } catch (e) {
+        console.log(e);
+    }
+    return (Number(s1.replace('.', '')) * Number(s2.replace('.', ''))) / Math.pow(10, m);
+};
+
+// 除法获取精确值
+export const accDiv = (arg1, arg2) => {
+    var t1 = 0,
+        t2 = 0,
+        r1,
+        r2;
+    try {
+        t1 = arg1.toString().split('.')[1].length;
+    } catch (e) {
+        console.log(e);
+    }
+    try {
+        t2 = arg2.toString().split('.')[1].length;
+    } catch (e) {
+        console.log(e);
+    }
+    Math.r1 = Number(arg1.toString().replace('.', ''));
+    Math.r2 = Number(arg2.toString().replace('.', ''));
+    return (Math.r1 / Math.r2) * Math.pow(10, t2 - t1);
+};
